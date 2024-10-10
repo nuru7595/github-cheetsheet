8cc8ec9b91fc6f7105b6a77eaf5ecfd94e33c8b3
nuru7595 <nuruzzamannuru7595@gmail.com>
2024-10-10 17:34:30 +0600

    extra files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    container: {
+      center: true
+    },
+    fontFamily: {
+      ubuntu: ["Ubuntu", "sans-serif"]
+    }
+  },
+  plugins: [],
+}
+
